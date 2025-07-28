2e74d2112e893fe99680f648c984abeb3ea5b40d
tetu817 <122658251+tetu817@users.noreply.github.com>
2025-07-29 02:05:45 +0900

    Update shift_app.py

--- shift_app.py
+++ shift_app.py
@@ -509,12 +509,6 @@ if 'shift' in st.session_state:
     if 'FPDF' in globals():
         pdf = FPDF()
         pdf.add_page()
-        font_path = 'DejaVuSans.ttf'
-        if not os.path.exists(font_path):
-            url = 'https://github.com/dejavu-fonts/dejavu-fonts/raw/master/ttf/DejaVuSansCondensed.ttf'
-            r = requests.get(url)
-            with open(font_path, 'wb') as f:
-                f.write(r.content)
         pdf.add_font('DejaVu', '', 'DejaVuSans.ttf', uni=True)
         pdf.set_font('DejaVu', size=6)
         columns = ['日付', '小野', '宮村', '廣内', '応援', '人数']
